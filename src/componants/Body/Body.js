890008fe2f6d27955512e8b12cdb9b96396e650a
Ben Stidham <benjamindstidham@gmail.com>
2019-03-06 13:26:22 -0600

    added button

--- src/componants/Body/Body.js
+++ src/componants/Body/Body.js
@@ -11,12 +11,21 @@ class Body extends Component {
       number: this.state.number + 1
     });
   };
+  decreaseNumber = () => {
+    this.setState({
+      number: this.state.number - 1
+    });
+  };
   render() {
     return (
       <div className="outercontainer">
         <button className="bodybutton" onClick={() => this.increaseNumber()}>
           {" "}
           Increase number{" "}
+        </button>
+        <button className="bodybutton" onClick={() => this.decreaseNumber()}>
+          {" "}
+          Decrease number{" "}
         </button>
         {this.state.number}
       </div>
